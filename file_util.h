f690ebf879dbb4c2aec49c1d72a8980d5577a0a2
Yahya Adejoh <35204206+zahyaa@users.noreply.github.com>
2019-02-11 10:18:02 -0500

    Add files via upload

---
+++ file_util.h
@@ -0,0 +1,15 @@
+#ifndef FILE_UTIL_H
+#define FILE_UTIL_H
+
+    //code here
+
+
+void processFile(char inpFile[15], char outFile[15],char inExt[6],char outExt[6]);
+
+
+#endif
+
+//#ifndef IN.EXTENSION
+//#define IN.EXTENSION
+    //define other file types/extensions?
+//#endif
